030862bf823e413e6462aef1d2853618afdd9e74
pbeocanin <pbeocanin@gmail.com>
2019-05-18 21:32:40 +0200

    Homework

--- src/store/actions.js
+++ src/store/actions.js
@@ -7,3 +7,13 @@ export const changeName = data => ({
 	type: 'SOME_ACTION_TYPE',
 	name: data,
 });
+
+/*
+
+1) Create an interface to let me update user object
+2) Create a way  or the user object to persist through reload
+3) Store history for evey edit in an array of values, and show this
+4) Let me change edit history ORDER
+5) Store ALL that somehow through reload
+
+*/
