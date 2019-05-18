e77dd74d3a1d419f75fe95047b439d975dc66bca
pbeocanin <pbeocanin@gmail.com>
2019-05-18 18:19:58 +0200

    Added Provider to wrap the app in

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import styled from 'styled-components';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { store } from './store/';
 import { PageOne } from './pages/PageOne/';
 import PageTwo from './pages/PageTwo/';
 
@@ -16,20 +18,25 @@ class App extends React.Component {
 
 	render() {
 		return (
-			<div className="container">
-				<div className="row">
-					<div className="col-12">
-						<Wrapper>
-							<Router>
-								<Switch>
-									<Route path="/" component={PageOne} />
-									<Route path="/class" component={PageTwo} />
-								</Switch>
-							</Router>
-						</Wrapper>
+			<Provider store={store}>
+				<div className="container">
+					<div className="row">
+						<div className="col-12">
+							<Wrapper>
+								<Router>
+									<Switch>
+										<Route path="/" component={PageOne} />
+										<Route
+											path="/class"
+											component={PageTwo}
+										/>
+									</Switch>
+								</Router>
+							</Wrapper>
+						</div>
 					</div>
 				</div>
-			</div>
+			</Provider>
 		);
 	}
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React from 'react';
 import { createStore, combineReducers } from 'redux';
 import reducers from './reducers';
