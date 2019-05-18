f3c8b59a33a0f84f14336f67287c26d054d15936
pbeocanin <pbeocanin@gmail.com>
2019-05-18 18:10:25 +0200

    Added bootstrap wrap

--- src/index.js
+++ src/index.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import styled from 'styled-components';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { PageOne } from './pages/PageOne/';
 import PageTwo from './pages/PageTwo/';
+
+const Wrapper = styled.div`
+	height: 100vh;
+	display: flex;
+	align-items: center;
+`;
 
 class App extends React.Component {
 	state = {};
 
 	render() {
 		return (
-			<div>
-				<Router>
-					<Switch>
-						<Route path="/" component={PageOne} />
-						<Route path="/class" component={PageTwo} />
-					</Switch>
-				</Router>
+			<div className="container">
+				<div className="row">
+					<div className="col-12">
+						<Wrapper>
+							<Router>
+								<Switch>
+									<Route path="/" component={PageOne} />
+									<Route path="/class" component={PageTwo} />
+								</Switch>
+							</Router>
+						</Wrapper>
+					</div>
+				</div>
 			</div>
 		);
 	}
