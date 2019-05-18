35f13e71dd8318777c1d10c2df83e1daf8bc19ae
pbeocanin <pbeocanin@gmail.com>
2019-05-18 18:15:07 +0200

    Added a store

---
+++ src/store/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { createStore, combineReducers } from 'redux';
+import reducers from '../reducers/';
+
+export const createStore = createStore(
+	combineReducers({ state: reducers }),
+	window.__REDUX_DEVTOOLS_EXTENSION__ &&
+		window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+
+// What are we doing? Creating a global state basically. The window.__REDUX
+// is simply for redux chrome extension. If you don't wanna use it, you can not
+// include the code, or simply leave it since it only works if there is one
